7f2202e4b917bf0569c741706fb18211ddcc336f
maddox blay <41036597-maddoxki800@users.noreply.replit.com>
2025-05-29 15:20:51 +0000

    created lib and board.rb and added require relative to main

---
+++ lib/board.rb
@@ -0,0 +1,11 @@
+def draw_board(_board) 
+  puts "     |     |     "
+  puts "  #{_board[0][0]}  |  #{_board[0][1]}  |  #{_board[0][2]}  "
+  puts "_____|_____|_____"
+  puts "     |     |     "
+  puts "  #{_board[1][0]}  |  #{_board[1][1]}  |  #{_board[1][2]}  "
+  puts "_____|_____|_____"
+  puts "     |     |     "
+  puts "  #{_board[2][0]}  |  #{_board[2][1]}  |  #{_board[2][2]}  "
+  puts "     |     |     "
+end
